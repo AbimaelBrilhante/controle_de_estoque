8e693ab04d8d3e18648f681d6fbaab10e48cf6d2
Abimael Brilhante <88044370+AbimaelBrilhante@users.noreply.github.com>
2022-09-22 11:29:43 -0300

    codigo refatorado
    adicionado informações ao __init__

--- controle_de_estoque.py
+++ controle_de_estoque.py
@@ -1,25 +1,21 @@
 import sqlite3
-import pandas as pd
 import csv
 import os
 
-print('[1] - Cadastrar Produto \n' 
-      '[2] - Cadastrar Entrada de Produto \n' 
-      '[3] - Cadastrar Saída de Produto \n' 
-      '[4] - Consultar Movimentação de Estoque \n' 
-      '[5] - Consultar Saldo de estoque \n' 
-      '[6] - Editar Produto \n'
-      '[7] - Excluir Produto \n' 
-      '[8] - Excluir Entrada/Saida \n' 
-      '[9] - Exportar Ficha de Estoque \n')
-
-
-
 class Inventario:
 
-    def __init__(self,arquivo):
+    def __init__(self, arquivo):
         self.conexao = sqlite3.connect(arquivo)
         self.cursor = self.conexao.cursor()
+        print('[1] - Cadastrar Produto \n'
+              '[2] - Cadastrar Entrada de Produto \n'
+              '[3] - Cadastrar Saída de Produto \n'
+              '[4] - Consultar Movimentação de Estoque \n'
+              '[5] - Consultar Saldo de estoque \n'
+              '[6] - Editar Produto \n'
+              '[7] - Excluir Produto \n'
+              '[8] - Excluir Entrada/Saida \n'
+              '[9] - Exportar Ficha de Estoque \n')
 
     def criar_tabelas(self):
         tabela_materiais = 'CREATE TABLE IF NOT EXISTS dados_materiais(cod_produto text,descricao_produto text,' \
@@ -29,12 +25,12 @@ class Inventario:
         self.cursor.execute(tabela_materiais)
         self.cursor.execute(movimentacao_estoque)
 
-    def cadastrar_produto(self,cod_produto,*args):
+    def cadastrar_produto(self, cod_produto, *args):
         inserir = 'INSERT OR IGNORE INTO dados_materiais (cod_produto,descricao_produto,unidade_de_medida,cor,tamanho) values(?,?,?,?,?)'
         self.cursor.execute(inserir, (cod_produto, *args))
         self.conexao.commit()
 
-    def verificar_cadastro(self,cod_produto):
+    def verificar_cadastro(self, cod_produto):
         produtos_cadastrados = []
         consulta = 'SELECT cod_produto,descricao_produto,unidade_de_medida,cor,tamanho FROM dados_materiais WHERE cod_produto LIKE ?'
         self.cursor.execute(consulta, (f'%{cod_produto}%',))
@@ -47,40 +43,40 @@ class Inventario:
         else:
             pass
 
-    def estoque(self,cod_produto,*args):
+    def estoque(self, cod_produto, *args):
         estoque = 'INSERT OR IGNORE INTO inventario_relatorio (cod_produto,descricao_produto,' \
-                    'quantidade,data,numero_documento,movimento) VALUES(?,?,?,?,?,?)'
+                  'quantidade,data,numero_documento,movimento) VALUES(?,?,?,?,?,?)'
         self.cursor.execute(estoque, (cod_produto, *args))
         self.conexao.commit()
 
-    def consultar_movimentacao(self,cod_produto):
+    def consultar_movimentacao(self, cod_produto):
         consulta = 'SELECT id,cod_produto,descricao_produto,quantidade, movimento FROM inventario_relatorio WHERE cod_produto LIKE ?'
         self.cursor.execute(consulta, (f'%{cod_produto}%',))
         for linha in self.cursor.fetchall():
             print(linha)
 
-    def consultar_saldo(self,cod_produto):
+    def consultar_saldo(self, cod_produto):
         saldo = 0
         consulta = 'SELECT cod_produto,descricao_produto,quantidade, movimento FROM inventario_relatorio WHERE cod_produto LIKE ?'
         self.cursor.execute(consulta, (f'%{cod_produto}%',))
         for linha in self.cursor.fetchall():
-            saldo+=linha[2]
+            saldo += linha[2]
         print(f'Saldo em estoque do produto {linha[0]} - {linha[1]}: {saldo}')
 
-    def editar_produto(self,cod_produto, descricao_produto,unidade_de_medida,cor,tamanho):
+    def editar_produto(self, cod_produto, descricao_produto, unidade_de_medida, cor, tamanho):
         consulta = 'UPDATE dados_materiais SET descricao_produto=?,unidade_de_medida=?,' \
                    'cor=?,tamanho=? WHERE cod_produto=?'
-        self.cursor.execute(consulta, (descricao_produto,unidade_de_medida,cor,tamanho,cod_produto))
+        self.cursor.execute(consulta, (descricao_produto, unidade_de_medida, cor, tamanho, cod_produto))
         self.conexao.commit()
         print(f'Produto {cod_produto} alterado com sucesso')
 
-    def excluir_produto(self,cod_produto):
+    def excluir_produto(self, cod_produto):
         consulta = 'DELETE FROM dados_materiais WHERE cod_produto=?'
         self.cursor.execute(consulta, (cod_produto,))
         self.conexao.commit()
         print(f'Produto {cod_produto} excluído com sucesso')
 
-    def excluir_entrada_saida(self,id):
+    def excluir_entrada_saida(self, id):
         consulta = 'DELETE FROM inventario_relatorio WHERE id=?'
         self.cursor.execute(consulta, (id,))
         self.conexao.commit()
@@ -96,10 +92,10 @@ class Inventario:
         for linha in self.cursor.fetchall():
             print(linha)
 
-
     def fechar_conexao(self):
         self.cursor.close()
         self.conexao.close()
+
 
 inventario_relatorio = Inventario('inventario_relatorio.db')
 inventario_relatorio.criar_tabelas()
@@ -111,24 +107,24 @@ if opcao == "9":
 
 continuar = "s"
 while continuar == "s":
-    if opcao =="1":
+    if opcao == "1":
         inventario_relatorio.cadastrar_produto(input("Código do Produto: "), input("Descrição do Produto: "),
-                                               input("Unidade de Medida: "),input("Cor do Produto: "),
+                                               input("Unidade de Medida: "), input("Cor do Produto: "),
                                                input("Tamanho do Produto: "))
         continuar = input("cadastrar novo produto? s/n ")
-    elif opcao =="2":
+    elif opcao == "2":
         digitacao = input("Código do Produto: ")
         inventario_relatorio.verificar_cadastro(digitacao)
         inventario_relatorio.estoque(digitacao, input("Descrição do Produto: "),
                                      float(input("Quantidade do Produto: ")), input("Data: "),
-                                     input("Número do documento de Entrada: "),"ENTRADA")
+                                     input("Número do documento de Entrada: "), "ENTRADA")
         continuar = input("cadastrar nova entrada? s/n ")
     elif opcao == "3":
         digitacao = input("Código do Produto: ")
         inventario_relatorio.verificar_cadastro(digitacao)
         inventario_relatorio.estoque(digitacao, input("Descrição do Produto: "),
-                                     float(input("Quantidade do Produto: "))*-1, input("Data: "),
-                                     input("Número do documento de Saída: "),"SAIDA")
+                                     float(input("Quantidade do Produto: ")) * -1, input("Data: "),
+                                     input("Número do documento de Saída: "), "SAIDA")
         continuar = input("cadastrar nova saída? s/n ")
     elif opcao == "4":
         inventario_relatorio.consultar_movimentacao(input("Código do Produto: "))
@@ -137,8 +133,8 @@ while continuar == "s":
         inventario_relatorio.consultar_saldo(input("Código do Produto: "))
         continuar = input("Consultar outro produto ? s/n ")
     elif opcao == "6":
-        inventario_relatorio.editar_produto(input("Código do Produto: "),input("Descrição do Produto: "),
-                                                  input("Unidade de Medida: "),input("Cor: "),input("Tamanho: "))
+        inventario_relatorio.editar_produto(input("Código do Produto: "), input("Descrição do Produto: "),
+                                            input("Unidade de Medida: "), input("Cor: "), input("Tamanho: "))
         continuar = input("Editar outro produto ? s/n ")
     elif opcao == "7":
         inventario_relatorio.excluir_produto(input("Código do Produto: "))
@@ -147,17 +143,10 @@ while continuar == "s":
         inventario_relatorio.excluir_entrada_saida(input("Id a ser excluído: "))
         continuar = input("Excluir outro(a) entrada/saída ? s/n ")
 
-
 """    else:
         print('Digite uma opção válida')"""
 
 
-
-
-
-
-#2EXPORTAR FICHA DE CADASTRO EM EXCEL ORGANIZADO
-#3FORMATAÇÃO DA DATA
-#4LINKAR TABELA DO INVENTÁRIO COM BD
-
+# 3FORMATAÇÃO DA DATA
+# 4LINKAR TABELA DO INVENTÁRIO COM BD
 
